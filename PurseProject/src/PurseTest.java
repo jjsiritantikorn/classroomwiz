2b4840d57b135b9715f83f6ed16e45ecb3ad24bd
mmenafee <mmenafee@deerfield.edu>
2016-04-12 23:47:58 -0400

    update all test cases

--- PurseProject/src/PurseTest.java
+++ PurseProject/src/PurseTest.java
@@ -36,6 +36,7 @@ public class PurseTest {
 	
 	@Test
 	public void testAddCoin() {
+		p1.addCoin(quarter);
 		assertEquals("Purse should now .equals \"Purse[Quarter]\" ", "Purse[Quarter]", p1.toString());
 		p1.addCoin(dime);
 		assertEquals("Purse should now .equals \"Purse[Quarter,Dime]\" ", "Purse[Quarter,Dime]", p1.toString());
@@ -48,19 +49,27 @@ public class PurseTest {
 	
 	@Test
 	public void testReverse() {
-		assertEquals("Make sure your format is exactly as described", "Purse[Quarter,Nickel,Dime,Quarter]", p1.reverse());
+		p2.reverse();
+		assertEquals("Make sure your format is exactly as described", "Purse[Quarter,Nickel,Dime,Quarter]", p2.toString());
 	}
 	@Test
 	public void testTransfer() {
-		assertEquals("This purse should now have the other purse contents in it","Purse[Quarter,Dime,Nickel,Quarter]",p1.transfer(p2));
+		p1.transfer(p2);
+		assertEquals("This purse should now have the other purse contents in it","Purse[Nickel,Quarter]",p1.toString());
 	}
 	@Test
 	public void testSameContents(){
+		p1.sameContents(p2);
 		assertEquals("This purse should have the same contents as the tested purse","False",p1.sameContents(p2));
 	}
 	@Test
 	public void testSameCoins(){
-		assertEquals("This purse should have the same type of coins as the tested purse","False",p1.sameContents(p2));
+		p1.sameContents(p2);
+		assertEquals("This purse should have the same type of coins as the tested purse","False",p1.toString());
+		ArrayList<String> input1 = new ArrayList<String>(Arrays.asList("quarter", "dime","nickel","quarter"));
+		Purse p3 = new Purse(input1);
+		p3.sameContents(p3);
+		assertEquals("This purse should have the same type of coins as the tested purse","True",p3.toString());
 	}
 	
 
